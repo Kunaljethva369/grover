203b7489c1c10b4b595a16d7110092d0f721ce99
Kunal jethva <kunaljethva90@gmail.com>
2023-06-19 19:24:43 +0530

    overflow changes hidden

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ hamburger.addEventListener('click', function () {
     hamburger.childNodes[1].src == "https://cosmic-manatee-39bd99.netlify.app/Assets/cross.webp" ?
         hamburger.childNodes[1].src = "/Assets/hamburger.svg"
         :
-        hamburger.childNodes[1].src = "https://cosmic-manatee-39bd99.netlify.app/Assets/cross.webp"
-    document.body.style.overflow = 'hidden';
+        hamburger.childNodes[1].src = "https://cosmic-manatee-39bd99.netlify.app/Assets/cross.webp";
+        if(document.body.style.overflow == 'hidden' ){
+            document.body.style.overflow = 'auto';
+        }else{
+            document.body.style.overflow = 'hidden';
+        }
 });

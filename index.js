3bc84d89fd606c7da4fc292e9c527ef2f3ba59e0
Kunal jethva <kunaljethva90@gmail.com>
2023-06-19 16:52:52 +0530

    ui changes

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 let hamburger = document.getElementById('hamMenu');
 let menu = document.getElementById('menu');
-let cross = document.getElementById('cross');
 let ul = document.querySelector('.menu-items');
 
 hamburger.addEventListener('click', function () {
-    menu.className == 'menu' ? menu.className = 'menu active' : ''
-
+    menu.className == 'menu' ? menu.className = 'menu active' : menu.className = 'menu'
     menu.className == 'menu active' ? ul.className = 'flex menu-ul' : ul.className = 'flex menu-ul'
-
-    hamburger.style.display = 'none';
+    hamburger.childNodes[1].src == "http://127.0.0.1:5500/Assets/cross.webp" ?
+        hamburger.childNodes[1].src = "http://127.0.0.1:5500/Assets/hamburger.svg"
+        :
+        hamburger.childNodes[1].src = "http://127.0.0.1:5500/Assets/cross.webp"
     document.body.style.overflow = 'hidden';
-    cross.style.display = 'flex'
-});
-
-cross.addEventListener('click', function () {
-    menu.className == 'menu active' ? menu.className = 'menu' : ''
-    hamburger.style.display = 'flex';
-    document.body.style.overflow = 'auto';
-    cross.style.display = 'none';
 });

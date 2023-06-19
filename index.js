74630c61b2c71cbb5c3588566344980b0a4f473e
Kunal jethva <kunaljethva90@gmail.com>
2023-06-19 19:20:24 +0530

    local port change

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ let ul = document.querySelector('.menu-items');
 hamburger.addEventListener('click', function () {
     menu.className == 'menu' ? menu.className = 'menu active' : menu.className = 'menu'
     menu.className == 'menu active' ? ul.className = 'flex menu-ul' : ul.className = 'flex menu-ul'
-    hamburger.childNodes[1].src == "http://127.0.0.1:5500/Assets/cross.webp" ?
-        hamburger.childNodes[1].src = "http://127.0.0.1:5500/Assets/hamburger.svg"
+    hamburger.childNodes[1].src == "/Assets/cross.webp" ?
+        hamburger.childNodes[1].src = "/Assets/hamburger.svg"
         :
-        hamburger.childNodes[1].src = "http://127.0.0.1:5500/Assets/cross.webp"
+        hamburger.childNodes[1].src = "/Assets/cross.webp"
     document.body.style.overflow = 'hidden';
 });

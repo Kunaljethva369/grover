89c9cc59c9a0f9f4a45f676edbae758a217426aa
kunal jethva <kunaljethva90@gmail.com>
2023-08-27 14:00:40 +0530

    change cross image

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ let ul = document.querySelector('.menu-items');
 hamburger.addEventListener('click', function () {
     menu.className == 'menu' ? menu.className = 'menu active' : menu.className = 'menu'
     menu.className == 'menu active' ? ul.className = 'flex menu-ul' : ul.className = 'flex menu-ul'
-    hamburger.childNodes[1].src == "https://cosmic-manatee-39bd99.netlify.app/Assets/cross.webp" ?
+    hamburger.childNodes[1].src == "https://grovers.netlify.app//Assets/cross.webp" ?
         hamburger.childNodes[1].src = "/Assets/hamburger.svg"
         :
-        hamburger.childNodes[1].src = "https://cosmic-manatee-39bd99.netlify.app/Assets/cross.webp";
+        hamburger.childNodes[1].src = "https://grovers.netlify.app/Assets/cross.webp";
         if(document.body.style.overflow == 'hidden' ){
             document.body.style.overflow = 'auto';
         }else{
